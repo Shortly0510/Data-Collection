b8c2e69a843078acf6c0ae25a63c09246313b989
Shortly0510 <91367917+Shortly0510@users.noreply.github.com>
2025-11-27 20:50:25 +0700

    Update Gabut.java

--- Gabut.java
+++ Gabut.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gabut;
 
 import java.util.Scanner;
 
-/**
- *
- * @author santoiyan
- */
 public class Gabut {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner in = new Scanner(System.in);
@@ -49,13 +38,13 @@ public class Gabut {
                         data[i] = data[i+1];
                         data[i+1] = temp;
                     }
-                    //for (int j = 0; j < jumlah - 1 - i; j++) {
-                        //if (data[j][0].compareTo(data[j+1][0]) > 0) {
-                            //String[] temp = data[j];
-                            //data[j] = data[j+1];
-                            //data[j+1] = temp;
-                        //}
-                    //}
+                    /**for (int j = 0; j < jumlah - 1 - i; j++) {
+                        if (data[j][0].compareTo(data[j+1][0]) > 0) {
+                            String[] temp = data[j];
+                            data[j] = data[j+1];
+                            data[j+1] = temp;
+                        }
+                    }*/
                 }
                 System.out.println("Data berhasil diurutkan");
             }
@@ -99,3 +88,4 @@ public class Gabut {
         } while (menu >= 1 && menu <= 4);
     }
 }
+
